f2956b639e0cb1888bf3f7e7b6188d9441e7e132
DELL <mahmoud180280@fci.bu.edu.eg>
2024-07-08 20:59:57 +0300

    new posts

---
+++ lib/features/filter/presentation/view/screens/filter_screen.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+import 'package:travel_system/core/helper/app_size_config.dart';
+import 'package:travel_system/features/new_posts/presentation/view/new_posts_screen/widgets/new_post_row_date_widget.dart';
+import 'package:travel_system/styles/assets/asset_manager.dart';
+import 'package:travel_system/styles/colors/color_manager.dart';
+import 'package:travel_system/styles/text_styles/text_styles.dart';
+import 'package:travel_system/styles/widets/default_button.dart';
+import 'package:travel_system/styles/widets/default_text_field.dart';
+
+class FilterScreen extends StatelessWidget {
+   FilterScreen({super.key});
+
+  var textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text('Filter'),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: SizeConfig.width * 0.06,
+          ),
+          child:Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const NewPostRowDateWidget(icon: AssetsManager.airportImage, text:'Flights',),
+        
+              SizedBox(height: SizeConfig.height * 0.01,),
+        
+              Container(
+                padding: EdgeInsets.only(
+                    left: SizeConfig.height * 0.02,
+                    right: SizeConfig.height * 0.02
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.grey[300],
+                ),
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton<String>(
+                    hint: Text('Enter Your Location',style: TextStyles.textStyle18Regular.copyWith(
+                        color: Colors.black54
+                    )),
+                    icon: const Icon(Icons.arrow_drop_down),
+                    elevation: 5,
+                    style: const TextStyle(color: Colors.black, fontSize: 16),
+                    onChanged: (String? newValue) {},
+                    items: <String>['One', 'Two', 'Three', 'Four']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ),
+        
+              SizedBox(height: SizeConfig.height * 0.04,),
+        
+              const NewPostRowDateWidget(icon: AssetsManager.travelDateImage, text:'Date time',),
+        
+              SizedBox(height: SizeConfig.height * 0.01,),
+        
+              GestureDetector(
+                onTap: (){
+                  showDatePicker(
+                      context: context,
+                      firstDate: DateTime.now(),
+                      lastDate: DateTime(2030)
+                  ).whenComplete(() => print('Done'));
+                },
+                child: Container(
+                    padding: EdgeInsets.symmetric(
+                        vertical: SizeConfig.height * 0.01,
+                        horizontal: SizeConfig.width * 0.02
+                    ),
+                    width: SizeConfig.width,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Text('Choose Date',style: TextStyles.textStyle18Medium.copyWith(
+                        color: Colors.black54
+                    ),
+                    )),
+              ),
+        
+              SizedBox(height: SizeConfig.height * 0.04,),
+        
+              const NewPostRowDateWidget(icon: AssetsManager.willToFlyImage, text:'Willing to fly',),
+        
+              SizedBox(height: SizeConfig.height * 0.01,),
+        
+              GestureDetector(
+                onTap: (){
+                  showDatePicker(
+                      context: context,
+                      firstDate: DateTime.now(),
+                      lastDate: DateTime(2030)
+                  ).whenComplete(() => print('Done'));
+                },
+                child: Container(
+                    padding: EdgeInsets.symmetric(
+                        vertical: SizeConfig.height * 0.01,
+                        horizontal: SizeConfig.width * 0.02
+                    ),
+                    width: SizeConfig.width,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Text('Choose Date',style: TextStyles.textStyle18Medium.copyWith(
+                        color: Colors.black54
+                    ),
+                    )),
+              ),
+        
+              SizedBox(height: SizeConfig.height * 0.04,),
+        
+              const NewPostRowDateWidget(icon: AssetsManager.hoursImage, text:'Hours',),
+        
+              SizedBox(height: SizeConfig.height * 0.01,),
+        
+              DefaultTextField(
+                  controller: textController,
+                  hintText: 'Hours',
+                  validator: (v){},
+                  keyboardType: TextInputType.number,
+                  textInputAction: TextInputAction.done,
+                  fillColor: Colors.grey[300]
+              ),
+        
+              SizedBox(height: SizeConfig.height * 0.04,),
+        
+              Align(
+                alignment: Alignment.center,
+                child: DefaultButton(
+                    buttonText: 'Confirm',
+                    onPressed: (){},
+                    buttonColor: ColorManager.primaryBlue,
+                    large: true
+                ),
+              ),
+        
+            ],
+          )
+        ),
+      ),
+    );
+  }
+}
--- lib/features/new_posts/presentation/view/new_posts_screen/screens/new_posts_screen.dart
+++ lib/features/new_posts/presentation/view/new_posts_screen/screens/new_posts_screen.dart
@@ -4,17 +4,20 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:travel_system/core/helper/app_size_config.dart';
 import 'package:travel_system/features/new_posts/presentation/cubit/new_post_cubit.dart';
 import 'package:travel_system/features/new_posts/presentation/cubit/new_post_states.dart';
+import 'package:travel_system/features/new_posts/presentation/view/new_posts_screen/widgets/new_post_row_date_widget.dart';
+import 'package:travel_system/styles/assets/asset_manager.dart';
 import 'package:travel_system/styles/colors/color_manager.dart';
 import 'package:travel_system/styles/text_styles/text_styles.dart';
 import 'package:travel_system/styles/widets/default_text_field.dart';
 
 class NewPostsScreen extends StatelessWidget {
-   NewPostsScreen({super.key});
+   const NewPostsScreen({super.key});
   
 
   @override
   Widget build(BuildContext context) {
     return  SingleChildScrollView(
+      physics: BouncingScrollPhysics(),
       child: BlocProvider(
         create: (context) => NewPostCubit(),
         child: BlocConsumer<NewPostCubit,NewPostStates>(
@@ -30,10 +33,7 @@ class NewPostsScreen extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
 
-
-                  Text('I Have',style: TextStyles.textStyle18Bold.copyWith(
-                      color: ColorManager.black
-                  ),),
+                  const NewPostRowDateWidget(icon: AssetsManager.iHaveImage,text: 'I Have',),
 
                   SizedBox(height:  SizeConfig.height * 0.01,),
 
@@ -44,20 +44,17 @@ class NewPostsScreen extends StatelessWidget {
                     ),
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(8),
-                      color: Colors.white,
+                      color: Colors.grey[300],
                     ),
                     child: DropdownButtonHideUnderline(
                       child: DropdownButton<String>(
-                        hint: Text('Enter Your Location',style: TextStyles.textStyle18Medium.copyWith(
+                        hint: Text('Enter Your Location',style: TextStyles.textStyle18Regular.copyWith(
                           color: Colors.black54
                         )),
                         icon: const Icon(Icons.arrow_drop_down),
-                        iconSize: 24,
-                        elevation: 16,
+                        elevation: 5,
                         style: const TextStyle(color: Colors.black, fontSize: 16),
-                        onChanged: (String? newValue) {
-
-                        },
+                        onChanged: (String? newValue) {},
                         items: <String>['One', 'Two', 'Three', 'Four']
                             .map<DropdownMenuItem<String>>((String value) {
                           return DropdownMenuItem<String>(
@@ -71,9 +68,7 @@ class NewPostsScreen extends StatelessWidget {
 
                   SizedBox(height:  SizeConfig.height * 0.02,),
 
-                  Text('Hours',style: TextStyles.textStyle18Bold.copyWith(
-                      color: ColorManager.black
-                  ),),
+                  const NewPostRowDateWidget(icon: AssetsManager.hoursImage,text: 'Hours',),
 
                   SizedBox(height:  SizeConfig.height * 0.01,),
 
@@ -86,7 +81,7 @@ class NewPostsScreen extends StatelessWidget {
                             validator: (v){},
                             keyboardType: TextInputType.number,
                             textInputAction: TextInputAction.done,
-                            fillColor: Colors.white
+                            fillColor: Colors.grey[300]
                         ),
                       ),
 
@@ -99,7 +94,7 @@ class NewPostsScreen extends StatelessWidget {
                             validator: (v){},
                             keyboardType: TextInputType.number,
                             textInputAction: TextInputAction.done,
-                            fillColor: Colors.white
+                            fillColor: Colors.grey[300]
                         ),
                       ),
                     ],
@@ -107,9 +102,39 @@ class NewPostsScreen extends StatelessWidget {
 
                   SizedBox(height:  SizeConfig.height * 0.02,),
 
-                  Text('Date Time',style: TextStyles.textStyle18Bold.copyWith(
-                      color: ColorManager.black
-                  ),),
+                  const NewPostRowDateWidget(icon: AssetsManager.travelDateImage,text: 'Date Time',),
+
+                  SizedBox(height:  SizeConfig.height * 0.01,),
+
+                  GestureDetector(
+                    onTap: (){
+                      showDatePicker(
+                          context: context,
+                          firstDate: DateTime.now(),
+                          lastDate: DateTime(2030)
+                      ).whenComplete(() => print('Done'));
+                    },
+                    child: Container(
+                        padding: EdgeInsets.symmetric(
+                            vertical: SizeConfig.height * 0.015,
+                            horizontal: SizeConfig.width * 0.02
+                        ),
+                        width: SizeConfig.width,
+                        decoration: BoxDecoration(
+                          color: Colors.grey[300],
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Text('Choose Date',style: TextStyles.textStyle18Medium.copyWith(
+                          color: Colors.black54
+                        ),
+                        )),
+                  ),
+
+
+
+                  SizedBox(height:  SizeConfig.height * 0.02,),
+
+                  const NewPostRowDateWidget(icon: AssetsManager.startDateImage,text: 'Report Start Time',),
 
                   SizedBox(height:  SizeConfig.height * 0.01,),
 
@@ -128,47 +153,11 @@ class NewPostsScreen extends StatelessWidget {
                         ),
                         width: SizeConfig.width,
                         decoration: BoxDecoration(
-                          color: Colors.white,
+                          color: Colors.grey[300],
                           borderRadius: BorderRadius.circular(10),
                         ),
                         child: Text('Choose Date',style: TextStyles.textStyle18Medium.copyWith(
-                          color: Colors.grey
-                        ),
-                        )),
-                  ),
-
-
-
-
-                  SizedBox(height:  SizeConfig.height * 0.02,),
-
-                  Text('Report Start Time',style: TextStyles.textStyle18Bold.copyWith(
-                      color: ColorManager.black
-                  ),),
-
-                  SizedBox(height:  SizeConfig.height * 0.01,),
-
-
-                  GestureDetector(
-                    onTap: (){
-                      showDatePicker(
-                          context: context,
-                          firstDate: DateTime.now(),
-                          lastDate: DateTime(2030)
-                      ).whenComplete(() => print('Done'));
-                    },
-                    child: Container(
-                        padding: EdgeInsets.symmetric(
-                            vertical: SizeConfig.height * 0.01,
-                            horizontal: SizeConfig.width * 0.02
-                        ),
-                        width: SizeConfig.width,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Text('Choose Date',style: TextStyles.textStyle18Medium.copyWith(
-                            color: Colors.grey
+                            color: Colors.black54
                         ),
                         )),
                   ),
@@ -176,9 +165,7 @@ class NewPostsScreen extends StatelessWidget {
 
                   SizedBox(height:  SizeConfig.height * 0.02,),
 
-                  Text('Release End Time',style: TextStyles.textStyle18Bold.copyWith(
-                      color: ColorManager.black
-                  ),),
+                  const NewPostRowDateWidget(icon: AssetsManager.endDateImage,text: 'Release End Time',),
 
                   SizedBox(height:  SizeConfig.height * 0.01,),
 
@@ -197,11 +184,11 @@ class NewPostsScreen extends StatelessWidget {
                         ),
                         width: SizeConfig.width,
                         decoration: BoxDecoration(
-                          color: Colors.white,
+                          color: Colors.grey[300],
                           borderRadius: BorderRadius.circular(10),
                         ),
                         child: Text('Choose Date',style: TextStyles.textStyle18Medium.copyWith(
-                          color: Colors.grey
+                          color: Colors.black54
                         ),
                         )),
                   ),
@@ -209,10 +196,9 @@ class NewPostsScreen extends StatelessWidget {
 
                   SizedBox(height:  SizeConfig.height * 0.02,),
 
+                  const NewPostRowDateWidget(icon: AssetsManager.iWantImage,text: 'I Want',),
 
-                  Text('I Want',style: TextStyles.textStyle18Bold.copyWith(
-                      color: ColorManager.black
-                  ),),
+                  SizedBox(height:  SizeConfig.height * 0.01,),
 
                   Container(
                     padding: EdgeInsets.only(
@@ -221,7 +207,7 @@ class NewPostsScreen extends StatelessWidget {
                     ),
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(8),
-                      color: Colors.white,
+                      color: Colors.grey[300],
                     ),
                     child: DropdownButtonHideUnderline(
                       child: DropdownButton<String>(
@@ -248,10 +234,9 @@ class NewPostsScreen extends StatelessWidget {
 
                   SizedBox(height:  SizeConfig.height * 0.02,),
 
-                  Text('Willing to fly',style: TextStyles.textStyle18Bold.copyWith(
-                      color: ColorManager.black
-                  ),),
+                  const NewPostRowDateWidget(icon: AssetsManager.willToFlyImage,text: 'Willing to fly',),
 
+                  SizedBox(height:  SizeConfig.height * 0.01,),
 
                   GestureDetector(
                     onTap: (){
@@ -268,17 +253,17 @@ class NewPostsScreen extends StatelessWidget {
                         ),
                         width: SizeConfig.width,
                         decoration: BoxDecoration(
-                          color: Colors.white,
+                          color: Colors.grey[300],
                           borderRadius: BorderRadius.circular(10),
                         ),
                         child: Text('Choose Date',style: TextStyles.textStyle18Medium.copyWith(
-                          color: Colors.grey
+                          color: Colors.black54
                         ),
                         )),
                   ),
 
 
-                  SizedBox(height:  SizeConfig.height * 0.02,),
+                  SizedBox(height:  SizeConfig.height * 0.1,),
 
                 ],
               ),
