876c1fd107c4f2e841204bbdabb77322c26c314e
mohamed-mmdouh156 <mmdouh112@gmail.com>
2024-08-10 12:50:23 +0300

    add notification logic

--- lib/features/settings/presentation/view/notification_screen/widgets/notification_item.dart
+++ lib/features/settings/presentation/view/notification_screen/widgets/notification_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:travel_system/core/helper/app_size_config.dart';
+import 'package:travel_system/features/post_info/presentation/screens/post_details.dart';
 import 'package:travel_system/features/posts/data/models/post_model.dart';
 import 'package:travel_system/styles/colors/color_manager.dart';
 import 'package:travel_system/styles/text_styles/text_styles.dart';
@@ -11,38 +12,43 @@ class NotificationItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: SizeConfig.width,
+    return InkWell(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context) => PostDetails(postModel: postModel)));
+      },
+      child: Container(
+        width: SizeConfig.width,
 
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(SizeConfig.height*0.01),
-        color: ColorManager.gray,
-      ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(SizeConfig.height*0.01),
+          color: ColorManager.gray,
+        ),
 
-      child: Padding(
-        padding: EdgeInsets.all(SizeConfig.height*0.01),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            /// Notification Title
-            const Icon(
-              Icons.notifications_active,
-              color: ColorManager.primaryBlue,
-            ),
+        child: Padding(
+          padding: EdgeInsets.all(SizeConfig.height*0.01),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              /// Notification Title
+              const Icon(
+                Icons.notifications_active,
+                color: ColorManager.primaryBlue,
+              ),
 
-            SizedBox(width: SizeConfig.height*0.01),
+              SizedBox(width: SizeConfig.height*0.01),
 
-            Expanded(
-              child: Text(
-                "${postModel.userName} add a new trip corresponding to your trip with $percentage%",
-                style: TextStyles.textStyle18Medium.copyWith(
-                    color: ColorManager.black,
-                    fontWeight: FontWeight.bold
+              Expanded(
+                child: Text(
+                  "${postModel.userName} add a new trip corresponding to your trip with $percentage%",
+                  style: TextStyles.textStyle18Medium.copyWith(
+                      color: ColorManager.black,
+                      fontWeight: FontWeight.bold
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
